58715a4b4d07f7701910ab2aae0cb9c556407c04
Delbiaggio Nicolas <delbiaggionicolas@nico-3.home>
2018-10-29 19:01:05 +0100

    Handling incorrect credentials for the login
      When the user enter wrong crendentials, a message is displayed and
      the form is emptied.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { AuthService } from '../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +10,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  isValid: boolean = true;
+
+  form: FormGroup;
+
+
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
-    private router: Router
-  ) { }
+    private router: Router,
+    fb: FormBuilder
+  ) { 
+    this.form = fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   ngOnInit() {
   }
@@ -24,8 +36,10 @@ export class LoginComponent implements OnInit {
         if(result){
           let returnedUrl = this.route.snapshot.queryParamMap.get('returnUrl');
           this.router.navigate([returnedUrl || '/']);
+        }else{
+          this.isValid = false;
         }
-        console.log(result);
+        this.form.reset();
     })
   }
 
