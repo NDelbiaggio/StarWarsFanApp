3fa36c36fcc226143794aa31c31c8e02eb9d6747
Delbiaggio Nicolas <delbiaggionicolas@nico-3.home>
2018-10-31 09:52:59 +0100

    CardView has been removed + some restructuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,16 @@ import { LoaderComponent } from './loader/loader.component';
 import { PaginationBarComponent } from './pagination-bar/pagination-bar.component';
 
 import { HeaderListComponent } from './lists/header-list/header-list.component';
-import { ResourceOverviewComponent } from './resource-overview/resource-overview.component';
-import { SpeciesCardsComponent } from './resources/species/species-cards/species-cards.component';
-import { CardViewComponent } from './lists/card-view/card-view.component';
+
+import { StarwarsResourcesComponent } from './resources/starwars-resources/starwars-resources.component';
+import { ResourceOverviewComponent } from './resources/resource-overview/resource-overview.component';
 
 import { PeopleListComponent } from './resources/people/people-list/people-list.component';
 import { PeopleCardsComponent } from './resources/people/people-cards/people-cards.component';
 import { PeopleTableComponent } from './resources/people/people-table/people-table.component';
 import { PersonDetailsComponent } from './resources/people/person-details/person-details.component';
 
+import { SpeciesCardsComponent } from './resources/species/species-cards/species-cards.component';
 import { SpeciesTableComponent } from './resources/species/species-table/species-table.component';
 import { SpeciesListComponent } from './resources/species/species-list/species-list.component';
 import { SpeciesDetailsComponent } from './resources/species/species-details/species-details.component';
@@ -42,7 +43,6 @@ import { StarshipDetailsComponent } from './resources/starships/starship-details
 
 import { FilmsListComponent } from './resources/films/films-list/films-list.component';
 import { FilmDetailsComponent } from './resources/films/film-details/film-details.component';
-import { StarwarsResourcesComponent } from './resources/starwars-resources/starwars-resources.component';
 
 @NgModule({
   declarations: [
@@ -73,7 +73,6 @@ import { StarwarsResourcesComponent } from './resources/starwars-resources/starw
     SpeciesTableComponent,
     PlanetsTableComponent,
     
-    CardViewComponent, //Might be removed
     PeopleCardsComponent,
     SpeciesCardsComponent,
     VehiclesCardsComponent,
